cdec71c8f5ff711b2799f7f771ed61cc53040ca2
xiongsongsong <xiongsongsong@gmail.com>
2017-09-17 17:25:24 +0800

    fix 事件名错误

--- static/home.js
+++ static/home.js
@@ -2,8 +2,8 @@ var socket = io('http://localhost:3001');
 socket.on('connect', function () {
   console.log('connect')
 });
-socket.on('event', function (data) {
-  console.log('event', data)
+socket.on('ble', function (data) {
+  console.log(data)
 });
 socket.on('disconnect', function () {
   console.log('disconnect')
